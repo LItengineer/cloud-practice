3fd44b94965e251db0730d70f163bc793a2c847d
Wenke <dengwenke@koudailingqian.com>
2019-10-23 18:21:59 +0800

    add:初始化

---
+++ cloud-practice-web/src/main/java/com/wenke/CloudPracticeWebApplication.java
@@ -0,0 +1,18 @@
+package com.wenke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wenke
+ * @date 2019/10/23
+ */
+@SpringBootApplication
+public class CloudPracticeWebApplication {
+
+  public static void main(String[] args) {
+      SpringApplication.run(CloudPracticeWebApplication.class, args);
+  }
+
+
+}
